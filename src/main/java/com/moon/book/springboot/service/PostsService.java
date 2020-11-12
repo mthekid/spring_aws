abcdc8b041a219016f32efe92c3b230ec7d7080b
= <=>
2020-11-12 17:47:43 +0900

    chapter5 progress

--- src/main/java/com/moon/book/springboot/service/PostsService.java
+++ src/main/java/com/moon/book/springboot/service/PostsService.java
@@ -3,6 +3,7 @@ package com.moon.book.springboot.service;
 
 import com.moon.book.springboot.domain.posts.Posts;
 import com.moon.book.springboot.domain.posts.PostsRepository;
+import com.moon.book.springboot.web.dto.PostsListResponseDto;
 import com.moon.book.springboot.web.dto.PostsResponseDto;
 import com.moon.book.springboot.web.dto.PostsSaveRequestsDto;
 import com.moon.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,11 +34,26 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new
                         IllegalArgumentException("해당 게시글이 없습니다. id = " + id ));
 
         return new PostsResponseDto(entity);
+    }
+
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
